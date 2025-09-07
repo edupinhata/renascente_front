fdbd4714fe1b64b0fda087e8035be2e99bb6c541
edupinhata <edupinhata@gmail.com>
2025-09-07 00:01:29 -0300

    Fix some reserved keywords

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -90,9 +90,9 @@ function Home() {
         <div className="video-wrapper">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/eVSUR3puNzo?si=m3M81Yqu23u8ZiTE&amp;start=2812" 
             title="YouTube video player" 
-            frameborder="0" 
+            frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
           </iframe>
         </div>
       </section>
